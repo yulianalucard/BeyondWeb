7d220885426dac188f2149ec77dd87a1c666f3e8
yulianalucard <yulianalucard@gmail.com>
2025-12-26 17:10:33 +0000

    Unicor

--- beyondweb/beyondweb/settings.py
+++ beyondweb/beyondweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qy-yz6k*sc3&ctxz#k1sw%b!%%*sb!+@22swgd=a8=ndm#p8c2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
